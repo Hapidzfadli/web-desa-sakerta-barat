41e4eee7461ae2687d92965bb2b80ed7dfd06294
Hapid Fadli <hapidzfadli@gmail.com>
2024-07-02 23:02:37 +0700

    add cors frontend

--- backend-web-desa-sakerta-barat/src/main.ts
+++ backend-web-desa-sakerta-barat/src/main.ts
@@ -5,6 +5,11 @@ import { ErrorFilter } from './common/error.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new ErrorFilter());
+  app.enableCors({
+    origin: 'http://localhost:3000', // URL frontend Next.js Anda
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(3001);
 }
 bootstrap();
