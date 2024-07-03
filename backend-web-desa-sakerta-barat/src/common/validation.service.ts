63de8cea1691cf200332e3af5df996f2a8ad55b4
Hapid Fadli <hapidzfadli@gmail.com>
2024-07-03 22:42:31 +0700

    add api profile

--- backend-web-desa-sakerta-barat/src/common/validation.service.ts
+++ backend-web-desa-sakerta-barat/src/common/validation.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import { ZodType } from 'zod';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ZodError, ZodType } from 'zod';
 
 @Injectable()
 export class ValidationService {
   validate<T>(zodType: ZodType<T>, data: T): T {
-    return zodType.parse(data);
+    try {
+      return zodType.parse(data);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException({
+          message: 'Validation failed',
+          errors: error.errors,
+        });
+      }
+      throw error;
+    }
   }
 }
